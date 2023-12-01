d9264149de67b91c6fa0fd3f529dcf54286d7355
Champa Lal suthar <135296999+champalalsuthar@users.noreply.github.com>
2023-12-01 21:13:29 +0530

    Create App.js

---
+++ Quiz-App/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import Quiz from './components/Quiz'
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+     <Quiz/>
+    </div>
+  );
+}
+
+export default App;
