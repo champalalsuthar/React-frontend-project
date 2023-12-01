1f1a35451326a0c31f4baf0885456b81227adc05
Champa Lal suthar <135296999+champalalsuthar@users.noreply.github.com>
2023-12-01 21:11:50 +0530

    Create QuizData.jsx

---
+++ Quiz-App/QuizData.jsx
@@ -0,0 +1,97 @@
+export const QuizData = [
+  {
+    question: "What does HTML stand for?",
+    options: ["HyperText Markup Language", "Hyperlink and Text Markup Language", "High-Level Text Markup Language", "HyperTransfer Markup Language"],
+    answer: 1
+  },
+  {
+    question: "What is the primary purpose of CSS?",
+    options: ["To structure web content", "To provide interactivity", "To style and layout web content", "To handle server-side logic"],
+    answer: 3
+  },
+  {
+    question: "Which programming language is commonly used for client-side scripting in web development?",
+    options: ["Java", "Python", "JavaScript", "C++"],
+    answer: 3
+  },
+  {
+    question: "What is the purpose of the CSS box model?",
+    options: ["To define the layout of a webpage", "To create 3D effects", "To manage database queries", "To handle user authentication"],
+    answer: 1
+  },
+  {
+    question: "Which of the following is not a valid HTML tag?",
+    options: ["<section>", "<div>", "<article>", "<format>"],
+    answer: 4
+  },
+  {
+    question: "What does the acronym API stand for in web development?",
+    options: ["Application Programming Interface", "Advanced Programming Interface", "Automated Programming Interface", "Application Processing Interface"],
+    answer: 1
+  },
+  {
+    question: "Which version control system is widely used in web development for tracking changes in source code?",
+    options: ["Git", "SVN", "Mercurial", "Perforce"],
+    answer: 1
+  },
+  {
+    question: "What is the purpose of the HTTP protocol?",
+    options: ["To format web content", "To handle server-side logic", "To enable secure data transmission", "To define the structure of a database"],
+    answer: 3
+  },
+  {
+    question: "In web development, what does the term 'responsive design' refer to?",
+    options: ["Designing visually appealing websites", "Creating mobile applications", "Designing websites that adapt to different screen sizes", "Optimizing database queries"],
+    answer: 3
+  },
+  {
+    question: "What is the role of JavaScript frameworks like React and Angular?",
+    options: ["To manage server-side logic", "To handle database operations", "To enhance user interface interactivity", "To create web servers"],
+    answer: 3
+  },
+  {
+    question: "Which file extension is commonly used for Cascading Style Sheets?",
+    options: [".html", ".css", ".js", ".png"],
+    answer: 2
+  },
+  {
+    question: "What is the purpose of the 'alt' attribute in the HTML 'img' tag?",
+    options: ["To set the image source", "To define image dimensions", "To specify alternative text for the image", "To link to another webpage"],
+    answer: 3
+  },
+  {
+    question: "What is the primary use of the 'localStorage' object in JavaScript?",
+    options: ["To store data on the server", "To store data temporarily on the client-side", "To create cookies", "To manage user authentication"],
+    answer: 2
+  },
+  {
+    question: "Which of the following is a front-end JavaScript library for building user interfaces?",
+    options: ["Express", "Vue.js", "Django", "Flask"],
+    answer: 2
+  },
+  {
+    question: "What does the term 'SEO' stand for in web development?",
+    options: ["Search Engine Optimization", "Secure Encryption Operation", "Server Environment Overview", "Systematic Element Organization"],
+    answer: 1
+  },
+  {
+    question: "Which of the following is a server-side runtime environment for JavaScript?",
+    options: ["Node.js", "Angular", "React", "Vue.js"],
+    answer: 1
+  },
+  {
+    question: "What is the purpose of the 'DOCTYPE' declaration in HTML?",
+    options: ["To declare a JavaScript function", "To define the document type and version of HTML", "To create a new HTML document", "To specify the document font"],
+    answer: 2
+  },
+  {
+    question: "What is the role of the 'SQL' language in web development?",
+    options: ["To style web content", "To query and manipulate databases", "To create dynamic web pages", "To define webpage layouts"],
+    answer: 2
+  },
+  {
+    question: "Which HTML tag is used to create a hyperlink?",
+    options: ["<link>", "<a>", "<hlink>", "<url>"],
+    answer: 2
+  }
+];
